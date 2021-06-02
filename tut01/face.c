1b16113db4d4c55cbfc02cbd2de0c0b5fd9e4215
Ada Luong <ada.luong@student.unsw.edu.au>
2021-06-02 12:53:43 +1000

    added tut01 code

---
+++ tut01/face.c
@@ -0,0 +1,15 @@
+// Printing a face with double backslashes
+// Demonstration of escape sequences
+// Ada Luong, June 2021
+
+#include <stdio.h>
+
+int main (void) {
+
+	printf("~ ~\n");
+	printf("0 0\n");
+	printf(" o \n");
+	printf("\\\\_/\n"); // double backslash "\\" prints a single backslash "\"
+
+	return 0;
+}
---
+++ tut01/hello_world.c
@@ -0,0 +1,11 @@
+// Saying hi to the world
+// by Ada Luong, June 2021
+
+#include <stdio.h>
+
+int main (void) {
+	
+	printf("hello, world!\n");
+
+	return 0;
+}
