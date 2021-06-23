e08d1d1939afa59935fda3d627f9de25773fb345
Ada Luong <ada.luong@student.unsw.edu.au>
2021-06-23 16:20:44 +1000

    added extra resources for scanf

--- W11A/tut04/scanf.c
+++ W11A/tut04/scanf.c
@@ -5,16 +5,37 @@
 
 int main(void) {
 
-    int number;
+    // Scanning in one integer
 
+    int number_one;
     printf("Enter a number: ");
-    int result = scanf("%d", &number);
-    printf("%d\n", result);
+
+    int result_one = scanf("%d", &number_one);
+    printf("successfully scanned in %d value(s)\n", result_one);
+
+    if (result_one > 0) {
+        printf("the value scanned in was %d\n", number_one);
+    }
+
+    // Scanning in two integers
     
+    int number_two;
+    int number_three;
+
+    printf("\nThis example scans in two numbers.\nEnter two values: ");
+
+    int result_two = scanf("%d %d", &number_two, &number_three);
+    printf("successfully scanned in %d value(s)\n", result_two);
+
+    if (result_two > 0) {
+        printf("the values scanned in were %d %d\n", number_two, number_three);
+    }
+
     return 0;
 }
 
 /* VALUES TO TRY
  * an integer
  * a character
+ * EOF (CTRL-D)
  */ 
